91412e27f098899fc2f37d0c7b09faa84002e33b
ryota-ko <trb_right9@yahoo.co.jp>
2015-09-28 21:16:51 +0900

    first commit

---
+++ app/src/main/java/com/example/ryota_ko/sensorapp/MainActivity.java
@@ -0,0 +1,52 @@
+package com.example.ryota_ko.sensorapp;
+
+import android.app.Activity;
+import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.view.View;
+import android.widget.TextView;
+
+
+public class MainActivity extends Activity {
+
+    //現在のカウントを表示させるためのテキストビュー
+    TextView text;
+    //現在の値を保存しておくための変数
+    int number;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        text = (TextView) findViewById(R.id.textView);
+        text.setText("0");
+    }
+
+    public void plus(View v){
+        number = number + 1;
+        text.setText(String.valueOf(number));
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.menu_main, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        // Handle action bar item clicks here. The action bar will
+        // automatically handle clicks on the Home/Up button, so long
+        // as you specify a parent activity in AndroidManifest.xml.
+        int id = item.getItemId();
+
+        //noinspection SimplifiableIfStatement
+        if (id == R.id.action_settings) {
+            return true;
+        }
+
+        return super.onOptionsItemSelected(item);
+    }
+}
